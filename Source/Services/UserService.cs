b03b76462b5c6b468c8c5e237c59afceaee472ff
Romulo Paiva <romulofpaiva@gmail.com>
2020-07-08 22:51:24 -0300

    Buscar e salvar usuário

--- Source/Services/UserService.cs
+++ Source/Services/UserService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using Codenation.Challenge.Models;
 
@@ -5,8 +7,11 @@ namespace Codenation.Challenge.Services
 {
     public class UserService : IUserService
     {
+        CodenationContext codenationContext;
+
         public UserService(CodenationContext context)
         {
+            codenationContext = context;
         }
 
         public IList<User> FindByAccelerationName(string name)
@@ -17,16 +22,23 @@ namespace Codenation.Challenge.Services
         public IList<User> FindByCompanyId(int companyId)
         {
             throw new System.NotImplementedException();
+
+            // return codenationContext.Users
+            //     .Where(  )
+            //     .ToList();
         }
 
         public User FindById(int id)
         {
-            throw new System.NotImplementedException();
+            return codenationContext.Users.Find(id);
         }
 
         public User Save(User user)
         {
-            throw new System.NotImplementedException();
+            codenationContext.Users.Add(user);
+            codenationContext.SaveChanges();
+
+            return user;
         }
     }
 }
